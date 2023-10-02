60b56ca5e7efa310ff6646fa87814d5fafc1f76f
hardeymolar <79372525+hardeymolar@users.noreply.github.com>
2023-10-02 05:00:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json())
-app.options('*',cors());
+// app.options('*',cors());
 app.get('/',(req,res)=>{
     res.send('welcome to chromeXt api')
 });
